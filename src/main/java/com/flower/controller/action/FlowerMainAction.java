255fb432adb13613041057271dc9b6061a23ee20
ywshin0112 <ywshin0112@gmail.com>
2022-12-15 18:14:07 +0900

    집가기전

--- src/main/java/com/flower/controller/action/FlowerMainAction.java
+++ src/main/java/com/flower/controller/action/FlowerMainAction.java
@@ -1,6 +1,7 @@
 package com.flower.controller.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -9,7 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.flower.dao.FlowerCategoryDAO;
+import com.flower.dao.FlowerProductDAO;
+import com.flower.dao.FlowerProductImageDAO;
 import com.flower.vo.FlowerCategoryVO;
+import com.flower.vo.FlowerDetailListVO;
+import com.flower.vo.FlowerMainPageVO;
+import com.flower.vo.FlowerProductImageVO;
+import com.flower.vo.FlowerProductVO;
 
 public class FlowerMainAction implements Action {
 
@@ -17,10 +24,28 @@ public class FlowerMainAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = "flower/main.jsp";
 		
+		
+		
+		
+		//공통 header
 		FlowerCategoryDAO cdao = FlowerCategoryDAO.getInstance();
 		
 		List<FlowerCategoryVO> mainList = cdao.selectMainCategory();
 		request.setAttribute("mainList", mainList);
+		
+		
+		ArrayList<FlowerMainPageVO> mpvoList = new ArrayList<>();
+		
+		for(int i=0; i<mainList.size(); i++) {
+			String category = mainList.get(i).getCategory();
+			ArrayList<FlowerDetailListVO> dlvo = new ArrayList<>();
+			
+			
+			
+			
+		}
+		
+		
 
 		RequestDispatcher rd = request.getRequestDispatcher(url);
 		rd.forward(request, response);
